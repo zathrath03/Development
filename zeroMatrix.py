4be78b84d64c4ba58c3607c3ac43f04b0f4bfa3f
John Dugger <zathrath03@gmail.com>
2022-09-12 20:27:40 -0700

    Attempting an O(1) space implementation

--- zeroMatrix.py
+++ zeroMatrix.py
@@ -22,4 +22,35 @@ def zero_matrix(matrix: list[list]) -> None:
         for r in range(len(matrix)):
             matrix[r][col] = 0
 
+# This one is not currently working properly
+def zero_matrix_constant_space(matrix: list[list]) -> None:
+    first_row_has_zero = False
+    first_col_has_zero = False
+    for r in range(len(matrix)):
+        for c in range(len(matrix[0])):
+            if matrix[r][c] == 0:
+                if r == 0:
+                    first_row_has_zero = True
+                if c == 0:
+                    first_col_has_zero = True
+                if r and c:
+                    matrix [r][0] == 0
+                    matrix [0][c] == 0
+    for r in range(1, len(matrix)):
+        if matrix[r][0] == 0:
+            for c in range(len(matrix[0])):
+                matrix[r][c] = 0
+    for c in range(1, len(matrix[0])):
+        if matrix[0][c] == 0:
+            for r in range(len(matrix)):
+                matrix[r][c] = 0
+    if first_row_has_zero:
+        for c in range(len(matrix[0])):
+            matrix[0][c] = 0
+    if first_col_has_zero:
+        for r in range(len(matrix)):
+            matrix[r][0] = 0
 
+from random import randint
+def generate_matrix(r, c):
+    return [[randint(0,9) for _ in range(c)] for _ in range(r)]
