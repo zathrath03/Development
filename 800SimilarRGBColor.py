61c03d300473c53acce80834ae14dbf9182bff76
John Dugger <zathrath03@gmail.com>
2022-10-15 20:43:16 -0700

    Initial problem statement and function definition

---
+++ 800SimilarRGBColor.py
@@ -0,0 +1,18 @@
+"""
+The red-green-blue color "#AABBCC" can be written as "#ABC" in shorthand.
+
+For example, "#15c" is shorthand for the color "#1155cc".
+The similarity between the two colors "#ABCDEF" and "#UVWXYZ" is
+ -(AB - UV)2 - (CD - WX)2 - (EF - YZ)2.
+
+Given a string color that follows the format "#ABCDEF", return a string
+represents the color that is most similar to the given color and has a
+shorthand (i.e., it can be represented as some "#XYZ").
+
+Any answer which has the same highest similarity as the best answer will be
+accepted.
+"""
+
+
+def similarRGB(color: str) -> str:
+    pass
