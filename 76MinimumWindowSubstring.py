bfb26ecb75593f4a9be2ca7a33f6eaa00ef05739
John Dugger <zathrath03@gmail.com>
2022-10-22 08:57:48 -0700

    - Added constraints - Pseudocode concept for implementation

--- 76MinimumWindowSubstring.py
+++ 76MinimumWindowSubstring.py
@@ -4,9 +4,28 @@ Given two strings s and t of lengths m and n respectively, return the minimum wi
 The testcases will be generated such that the answer is unique.
 
 A substring is a contiguous sequence of characters within the string
+
+m == s.length
+n == t.length
+1 <= m, n <= 105
+s and t consist of uppercase and lowercase English letters.
 """
+
+
+from collections import Counter
 
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        pass
+        # Create a dict of char: count for the chars in t
+        t_counts = Counter(t)
+
+        # Find start of window
+        # Find end of window where all items in t are in the window
+        # Save window indexes as a possibility
+        # Shift the window
+        # # repeat the window search above on a substring
+        # # using the index after the current start as the start of the substring
+        # Return "" if no window indexes saved
+        # Check the saved window indexes for the smallest length
+        # Return the substring of the smallest length
