83526c8d06b6ad937d5a87dd440cb29074c3e515
John Dugger <zathrath03@gmail.com>
2022-10-25 18:41:44 -0700

    Initialize remainders to {0} so a subsequent multiple of k will return True - Passes all test cases

--- 523. Continuous Subarray Sum.py
+++ 523. Continuous Subarray Sum.py
@@ -18,7 +18,8 @@ import unittest
 class Solution:
     def checkSubarraySum(self, nums: list[int], k: int) -> bool:
         # if sum(nums[i:j])%k == 0, then sum(nums[:i])%k == sum(nums[:j])%k
-        remainders = set()
+        remainders = {0}
+        # TODO handle edge case where first num is a multiple of k
         running_sum = 0
         for num in nums:
             running_sum += num
@@ -26,7 +27,6 @@ class Solution:
             if remainder in remainders:
                 return True
             remainders.add(remainder)
-        return False
 
 
 class Test(unittest.TestCase):
