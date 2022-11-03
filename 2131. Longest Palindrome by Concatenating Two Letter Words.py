76ee28a4cf888f1c2e97b9d25382ddc2ee3fb5aa
John Dugger <zathrath03@gmail.com>
2022-11-03 15:49:26 -0700

    (2131) Added mypy typing

--- 2131. Longest Palindrome by Concatenating Two Letter Words.py
+++ 2131. Longest Palindrome by Concatenating Two Letter Words.py
@@ -30,40 +30,40 @@ class Solution:
         self.increment_output_if_double_letter_word_remains()
         return self.output
 
-    def init(self):
+    def init(self) -> None:
         self.needed_words: defaultdict[str, int] = defaultdict(int)
         self.output = 0
         self.double_letter_words = 0
 
-    def determine_if_needed(self, word):
+    def determine_if_needed(self, word: str) -> None:
         if word in self.needed_words:
             self.handle_needed_words(word)
         else:
             self.handle_new_words(word)
 
-    def increment_output_if_double_letter_word_remains(self):
+    def increment_output_if_double_letter_word_remains(self) -> None:
         if self.double_letter_words:
             self.output += 2
 
-    def handle_needed_words(self, word):
+    def handle_needed_words(self, word: str) -> None:
         self.decrement_double_letters(word)
         self.decrement_needed_words(word)
         self.output += 4
 
-    def handle_new_words(self, word):
+    def handle_new_words(self, word: str) -> None:
         self.increment_double_letters(word)
         self.needed_words[word[::-1]] += 1
 
-    def decrement_double_letters(self, word):
+    def decrement_double_letters(self, word: str) -> None:
         if word[0] == word[1]:
             self.double_letter_words -= 1
 
-    def decrement_needed_words(self, word):
+    def decrement_needed_words(self, word: str) -> None:
         self.needed_words[word] -= 1
         if self.needed_words[word] == 0:
             del self.needed_words[word]
 
-    def increment_double_letters(self, word):
+    def increment_double_letters(self, word: str) -> None:
         if word[0] == word[1]:
             self.double_letter_words += 1
 
