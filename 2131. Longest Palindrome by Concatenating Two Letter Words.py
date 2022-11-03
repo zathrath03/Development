ccca25b8b2d28554ea64cd10a6c47bf9da4ca11d
John Dugger <zathrath03@gmail.com>
2022-11-03 15:44:15 -0700

    (2131) Refactor - sequence the functions to read better

--- 2131. Longest Palindrome by Concatenating Two Letter Words.py
+++ 2131. Longest Palindrome by Concatenating Two Letter Words.py
@@ -30,9 +30,10 @@ class Solution:
         self.increment_output_if_single_double_letter_word()
         return self.output
 
-    def increment_output_if_single_double_letter_word(self):
-        if self.double_letter_words:
-            self.output += 2
+    def init(self):
+        self.needed_words: defaultdict[str, int] = defaultdict(int)
+        self.output = 0
+        self.double_letter_words = 0
 
     def determine_if_needed(self, word):
         if word in self.needed_words:
@@ -40,32 +41,31 @@ class Solution:
         else:
             self.handle_new_words(word)
 
-    def init(self):
-        self.needed_words: defaultdict[str, int] = defaultdict(int)
-        self.output = 0
-        self.double_letter_words = 0
+    def increment_output_if_single_double_letter_word(self):
+        if self.double_letter_words:
+            self.output += 2
+
+    def handle_needed_words(self, word):
+        self.decrement_double_letters(word)
+        self.decrement_needed_words(word)
+        self.output += 4
 
     def handle_new_words(self, word):
         self.increment_double_letters(word)
         self.needed_words[word[::-1]] += 1
 
-    def increment_double_letters(self, word):
+    def decrement_double_letters(self, word):
         if word[0] == word[1]:
-            self.double_letter_words += 1
+            self.double_letter_words -= 1
 
-    def handle_needed_words(self, word):
-        self.decrement_double_letters(word)
-        self.decrement_candidates(word)
-        self.output += 4
-
-    def decrement_candidates(self, word):
+    def decrement_needed_words(self, word):
         self.needed_words[word] -= 1
         if self.needed_words[word] == 0:
             del self.needed_words[word]
 
-    def decrement_double_letters(self, word):
+    def increment_double_letters(self, word):
         if word[0] == word[1]:
-            self.double_letter_words -= 1
+            self.double_letter_words += 1
 
 
 class Test(unittest.TestCase):
