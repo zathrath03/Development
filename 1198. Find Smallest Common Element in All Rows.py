091fbbf7da516d5ba593bc67bf5ac19f97b714ea
John Dugger <zathrath03@gmail.com>
2022-11-03 16:38:25 -0700

    (1198) Handle when a common element doesn't exist

--- 1198. Find Smallest Common Element in All Rows.py
+++ 1198. Find Smallest Common Element in All Rows.py
@@ -21,28 +21,28 @@ class Solution:
         num_rows = len(mat)
         num_cols = len(mat[0])
         ptr = [0] * num_rows
-        max_ptr = 0
         largest_current_value = mat[0][0]
         current_element = [0] * num_rows
 
-        while max_ptr < num_cols:
+        while True:
             for row in range(num_rows):
                 while (ptr[row] < num_cols
                        and mat[row][ptr[row]] < largest_current_value):
                     ptr[row] += 1
-                col = ptr[row]
+                if (col := ptr[row]) == num_cols:
+                    return -1
                 largest_current_value = current_element[row] = mat[row][col]
-                max_ptr = max(max_ptr, col)
                 if len(set(current_element)) == 1:
                     return largest_current_value
-        return 0
 
 
 class Test(unittest.TestCase):
     test_cases = [
         ([[1, 2, 3, 4, 5], [2, 4, 5, 8, 10],
           [3, 5, 7, 9, 11], [1, 3, 5, 7, 9]], 5),
-        [[[1, 2, 3], [2, 3, 4], [2, 3, 5]], 2]
+        [[[1, 2, 3], [2, 3, 4], [2, 3, 5]], 2],
+        ([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5],
+          [1, 2, 3, 4, 5], [6, 7, 8, 9, 10]], 0)
     ]
 
     def test_smallestCommonElement(self):
