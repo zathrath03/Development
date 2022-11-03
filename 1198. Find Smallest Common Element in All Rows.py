58366f229d17b069506e898e3dfcdc40a5bba01e
John Dugger <zathrath03@gmail.com>
2022-11-03 15:54:02 -0700

    (1198) Problem statement

---
+++ 1198. Find Smallest Common Element in All Rows.py
@@ -0,0 +1,12 @@
+"""
+Given an m x n matrix mat where every row is sorted in strictly increasing order, return the smallest common element in all rows.
+
+If there is no common element, return -1.
+
+Constraints:
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 500
+1 <= mat[i][j] <= 104
+mat[i] is sorted in strictly increasing order.
+"""
