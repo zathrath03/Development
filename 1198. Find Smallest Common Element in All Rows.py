565a4b94752f877babab7ada797363c7472eaa43
John Dugger <zathrath03@gmail.com>
2022-11-03 15:57:50 -0700

    (1198) Added provided test cases

--- 1198. Find Smallest Common Element in All Rows.py
+++ 1198. Find Smallest Common Element in All Rows.py
@@ -12,6 +12,26 @@ mat[i] is sorted in strictly increasing order.
 """
 
 
+import unittest
+
+
 class Solution:
     def smallestCommonElement(self, mat: list[list[int]]) -> int:
         return 0
+
+
+class Test(unittest.TestCase):
+    test_cases = [
+        ([[1, 2, 3, 4, 5], [2, 4, 5, 8, 10],
+          [3, 5, 7, 9, 11], [1, 3, 5, 7, 9]], 5),
+        [[1, 2, 3], [2, 3, 4], [2, 3, 5], 2]
+    ]
+
+    def test_smallestCommonElement(self):
+        sol = Solution()
+        for mat, expected in self.test_cases:
+            assert sol.smallestCommonElement(mat) == expected
+
+
+if __name__ == "__main__":
+    unittest.main()
