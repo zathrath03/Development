299cba5dfe546d4e5e870a226c7042e525c37fab
John Dugger <zathrath03@gmail.com>
2022-11-03 16:20:41 -0700

    (1198) Initial code - not passing tests

--- 1198. Find Smallest Common Element in All Rows.py
+++ 1198. Find Smallest Common Element in All Rows.py
@@ -27,6 +27,22 @@ class Solution:
          move the pointer to the next value until its equal or larger
          if larger, set the largest to that value and move to the next row
         """
+        num_rows = len(mat)
+        num_cols = len(mat[0])
+        ptr = [0] * num_rows
+        max_ptr = 0
+        largest_current_value = 0
+        current_element = [0] * num_rows
+
+        while max_ptr < num_cols:
+            for row in range(num_rows):
+                while (ptr[row] < num_cols
+                       and mat[row][ptr[row]] < largest_current_value):
+                    ptr[row] += 1
+                largest_current_value = current_element[row] = ptr[row]
+                max_ptr = max(max_ptr, ptr[row])
+                if len(set(current_element)) == 1:
+                    return largest_current_value
         return 0
 
 
