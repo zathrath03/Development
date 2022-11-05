69aec2112971aadb506dc2fab7286940a0b8c62f
John Dugger <zathrath03@gmail.com>
2022-11-05 10:42:30 -0700

    (212) Problem statement and function definition

---
+++ 212. Word Search II.py
@@ -0,0 +1,23 @@
+"""
+Given an m x n board of characters and a list of strings words, return all
+words on the board.
+
+Each word must be constructed from letters of sequentially adjacent cells,
+where adjacent cells are horizontally or vertically neighboring. The same
+letter cell may not be used more than once in a word.
+
+Constraints:
+m == board.length
+n == board[i].length
+1 <= m, n <= 12
+board[i][j] is a lowercase English letter.
+1 <= words.length <= 3 * 104
+1 <= words[i].length <= 10
+words[i] consists of lowercase English letters.
+All the strings of words are unique.
+"""
+
+
+class Solution:
+    def findWords(self, board: list[list[str]], words: list[str]) -> list[str]:
+        return []
