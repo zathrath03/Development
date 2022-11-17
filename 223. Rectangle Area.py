1ba1da6a8fc4b5b0020dfd165594aee352ad3ace
John Dugger <zathrath03@gmail.com>
2022-11-16 17:29:42 -0800

    (223) Problem statement and function definition

---
+++ 223. Rectangle Area.py
@@ -0,0 +1,21 @@
+"""
+Given the coordinates of two rectilinear rectangles in a 2D plane, return the
+total area covered by the two rectangles.
+
+The first rectangle is defined by its bottom-left corner (ax1, ay1) and its
+top-right corner (ax2, ay2).
+
+The second rectangle is defined by its bottom-left corner (bx1, by1) and its
+top-right corner (bx2, by2).
+
+Constraints:
+-104 <= ax1 <= ax2 <= 104
+-104 <= ay1 <= ay2 <= 104
+-104 <= bx1 <= bx2 <= 104
+-104 <= by1 <= by2 <= 104
+"""
+
+
+class Solution:
+    def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
+        return 0
