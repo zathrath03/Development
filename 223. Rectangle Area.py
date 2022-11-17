a10c8e90c6446f9c8a27928a590db25f2476b329
John Dugger <zathrath03@gmail.com>
2022-11-16 20:00:40 -0800

    (223) More comments. Added a simple, non-overlapping test case

--- 223. Rectangle Area.py
+++ 223. Rectangle Area.py
@@ -23,18 +23,19 @@ class Solution:
     def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int,
                     by1: int, bx2: int, by2: int) -> int:
         # Find area of each rectangle
-        # - (x2 - x1) * (y2 - y1)
+        # - (x2 - x1) * (y2 - y1) and sum them
         # Determine if there is overlap between the rectangles
-        # - bx1 < ax2 < bx2 and ((by1 <= ay1 < by2) or (by1 < ay2 <= by2))
-        # - by1 < ay2 < by2
-        # - bx1 < ax1 < bx2
-        # - by1 < ay1 < by2
+        # - (bx1 < ax2 < bx2 or bx1 < ax1 < bx2)
+        #     and (by1 <= ay1 < by2) or (by1 < ay2 <= by2)
+        # - (by1 < ay2 < by2 or by1 < ay1 < by2)
+        #     and (bx1 <= ax1 < bx2) or (bx1 < ax2 <= by2)
         # If there is overlap, subtract the area of the overlapping rectangle
         return 0
 
 
 class Test(unittest.TestCase):
     test_cases = [
+        ((0, 0, 2, 2, -2, -2, 0, 0, 8)),
         ((-3, 0, 3, 4, 0, -1, 9, 2), 45),
         ((-2, -2, 2, 2, -2, -2, 2, 2), 16)
     ]
