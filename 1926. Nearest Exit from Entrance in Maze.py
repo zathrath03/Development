e6223629f893f6bf907c931ea139848838a6bbf3
John Dugger <zathrath03@gmail.com>
2022-11-21 16:24:33 -0800

    (1926) Comments with my thought process

--- 1926. Nearest Exit from Entrance in Maze.py
+++ 1926. Nearest Exit from Entrance in Maze.py
@@ -29,6 +29,15 @@ import unittest
 
 class Solution:
     def nearestExit(self, maze: list[list[str]], entrance: list[int]) -> int:
+        # Use a breadth first search
+        # Keep track of visited cells (set) - add entrance
+        # Add the (entrance, 0) to a deque
+        # While there are items in the queue, pop each cell
+        # if the cell is not in visited and is adjacent to the end, return steps
+        # Find valid adjacent cells and add them to the queue with steps
+        # A valid square is within the bounds, has not been visited, and is "."
+        # Add cell to visited and repeat
+        # If we get to an empty queue, return -1
         return 0
 
 
