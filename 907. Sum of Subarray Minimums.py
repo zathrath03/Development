8a77b25237c9ed1bcba7266e0f051428a495133f
John Dugger <zathrath03@gmail.com>
2022-11-25 09:40:41 -0800

    (907) Problem statement and function definition

---
+++ 907. Sum of Subarray Minimums.py
@@ -0,0 +1,14 @@
+"""
+Given an array of integers arr, find the sum of min(b), where b ranges over
+every (contiguous) subarray of arr. Since the answer may be large, return the
+answer modulo 10^9 + 7.
+
+Constraints:
+1 <= arr.length <= 3 * 104
+1 <= arr[i] <= 3 * 104
+"""
+
+
+class Solution:
+    def sumSubarrayMins(self, arr: list[int]) -> int:
+        return 0
