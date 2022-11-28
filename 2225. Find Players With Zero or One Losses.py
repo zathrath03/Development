d2120762262922c3da8b3a3c09539007d6dde4ea
John Dugger <zathrath03@gmail.com>
2022-11-27 17:18:53 -0800

    (2225) Add given test cases

--- 2225. Find Players With Zero or One Losses.py
+++ 2225. Find Players With Zero or One Losses.py
@@ -23,6 +23,26 @@ All matches[i] are unique.
 """
 
 
+import unittest
+
+
 class Solution:
     def findWinners(self, matches: list[list[int]]) -> list[list[int]]:
         return [[0]]
+
+
+class Test(unittest.TestCase):
+    test_cases = (
+        ([[1, 3], [2, 3], [3, 6], [5, 6], [5, 7], [4, 5], [4, 8],
+         [4, 9], [10, 4], [10, 9]], [[1, 2, 10], [4, 5, 7, 8]]),
+        ([[2, 3], [1, 3], [5, 4], [6, 4]], [[1, 2, 5, 6], []]),
+    )
+
+    def test_findWinners(self):
+        sol = Solution()
+        for matches, expected in self.test_cases:
+            assert sol.findWinners(matches) == expected
+
+
+if __name__ == "__main__":
+    unittest.main()
