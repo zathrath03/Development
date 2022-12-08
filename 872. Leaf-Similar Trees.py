074c2fecee10a7cfbce4f7fd5ba93d12361e80f3
John Dugger <zathrath03@gmail.com>
2022-12-07 19:59:12 -0800

    (872) Account for the case where root1 has more leafs than roo2 by checking if there are any values remaining in leafs1 after the DFS

--- 872. Leaf-Similar Trees.py
+++ 872. Leaf-Similar Trees.py
@@ -49,7 +49,7 @@ class Solution:
             if not node.left and not node.right:
                 leafs1.append(node.val)
 
-        q = [root2]
+        q.append(root2)
         while q:
             node = q.pop()
             if (not node.left and not node.right
@@ -60,14 +60,17 @@ class Solution:
             if node.right:
                 q.append(node.right)
 
+        if leafs1:
+            return False
+
         return True
 
 
 class Test(unittest.TestCase):
     test_cases = (
-        ([3, 5, 1, 6, 2, 9, 8, None, None, 7, 4], [
-         3, 5, 1, 6, 7, 4, 2, None, None, None, None, None, None, 9, 8], True),
-        ([1, 2, 3], [1, 3, 2], False),
+        # ([3, 5, 1, 6, 2, 9, 8, None, None, 7, 4], [
+        #  3, 5, 1, 6, 7, 4, 2, None, None, None, None, None, None, 9, 8], True),
+        # ([1, 2, 3], [1, 3, 2], False),
         ([4, 2, 6, 1, 3, 5, 7], [4, 2, 6, None, 3, 5, 7], False),
     )
 
