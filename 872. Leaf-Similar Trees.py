19a1ca86c39858227e2a7e16a3a7911119a8901f
John Dugger <zathrath03@gmail.com>
2022-12-07 19:24:58 -0800

    (872) Add constraints to the problem statement

--- 872. Leaf-Similar Trees.py
+++ 872. Leaf-Similar Trees.py
@@ -15,6 +15,10 @@ the same.
 
 Return true if and only if the two given trees with head nodes root1 and root2
 are leaf-similar.
+
+Constraints:
+The number of nodes in each tree will be in the range [1, 200].
+Both of the given trees will have values in the range [0, 200].
 """
 
 
