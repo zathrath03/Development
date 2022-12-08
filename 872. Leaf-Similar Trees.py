805f9366fa0d985f464ca7ec88ba33275426c599
John Dugger <zathrath03@gmail.com>
2022-12-07 19:31:17 -0800

    (872) Add given test cases - reuse the deserialize method generated in (938)

--- 872. Leaf-Similar Trees.py
+++ 872. Leaf-Similar Trees.py
@@ -22,6 +22,9 @@ Both of the given trees will have values in the range [0, 200].
 """
 
 
+import unittest
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -33,3 +36,37 @@ class Solution:
     def leafSimilar(self, root1: TreeNode | None, root2: TreeNode | None
                     ) -> bool:
         return True
+
+
+class Test(unittest.TestCase):
+    test_cases = (
+        ([3, 5, 1, 6, 2, 9, 8, None, None, 7, 4], [
+         3, 5, 1, 6, 7, 4, 2, None, None, None, None, None, None, 9, 8], True),
+        ([1, 2, 3], [1, 3, 2], False)
+    )
+
+    def test_leafSimilar(self):
+        sol = Solution()
+        for root1, root2, expected in self.test_cases:
+            root1 = self.deserialize(root1)
+            root2 = self.deserialize(root2)
+            assert sol.leafSimilar(root1, root2) == expected
+
+    def deserialize(self, serialized_root):
+        if not serialized_root:
+            return None
+        nodes = [None if val is None else TreeNode(int(val))
+                 for val in serialized_root]
+        kids = nodes[::-1]
+        root = kids.pop()
+        for node in nodes:
+            if node:
+                if kids:
+                    node.left = kids.pop()
+                if kids:
+                    node.right = kids.pop()
+        return root
+
+
+if __name__ == "__main__":
+    unittest.main()
