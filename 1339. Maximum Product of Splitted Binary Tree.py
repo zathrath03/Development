12a387b5ef6068b1b2513a98607a041d388a6ace
John Dugger <zathrath03@gmail.com>
2022-12-10 12:00:15 -0800

    (1339) Add given test cases

--- 1339. Maximum Product of Splitted Binary Tree.py
+++ 1339. Maximum Product of Splitted Binary Tree.py
@@ -15,6 +15,9 @@ The number of nodes in the tree is in the range [2, 5 * 10^4].
 """
 
 
+import unittest
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -25,3 +28,35 @@ class TreeNode:
 class Solution:
     def maxProduct(self, root: TreeNode | None) -> int:
         return 0
+
+
+class Test(unittest.TestCase):
+    test_cases = (
+        ([1, 2, 3, 4, 5, 6], 110),
+        ([1, None, 2, 3, 4, None, None, 5, 6], 90),
+    )
+
+    def test_maxProduct(self):
+        sol = Solution()
+        for root, expected in self.test_cases:
+            root = self.deserialize(root)
+            assert sol.maxProduct(root) == expected
+
+    def deserialize(self, serialized_root):
+        if not serialized_root:
+            return None
+        nodes = [None if val is None else TreeNode(int(val))
+                 for val in serialized_root]
+        kids = nodes[::-1]
+        root = kids.pop()
+        for node in nodes:
+            if node:
+                if kids:
+                    node.left = kids.pop()
+                if kids:
+                    node.right = kids.pop()
+        return root
+
+
+if __name__ == "__main__":
+    unittest.main()
