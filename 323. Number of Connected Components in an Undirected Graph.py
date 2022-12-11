5933d7714923100bdf9b8fecabba46f453487d3b
John Dugger <zathrath03@gmail.com>
2022-12-10 18:30:10 -0800

    (323) Problem statement and function definition

---
+++ 323. Number of Connected Components in an Undirected Graph.py
@@ -0,0 +1,20 @@
+"""
+You have a graph of n nodes. You are given an integer n and an array edges
+where edges[i] = [ai, bi] indicates that there is an edge between ai and bi in
+the graph.
+
+Return the number of connected components in the graph.
+
+Constraints:
+1 <= n <= 2000
+1 <= edges.length <= 5000
+edges[i].length == 2
+0 <= ai <= bi < n
+ai != bi
+There are no repeated edges.
+"""
+
+
+class Solution:
+    def countComponents(self, n: int, edges: list[list[int]]) -> int:
+        return 0
