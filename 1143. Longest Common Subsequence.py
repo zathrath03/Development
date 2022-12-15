56e112eadef8fac5c9300173321e8a6f1ad6dda1
John Dugger <zathrath03@gmail.com>
2022-12-14 16:44:38 -0800

    (1143) Add test cases

--- 1143. Longest Common Subsequence.py
+++ 1143. Longest Common Subsequence.py
@@ -16,8 +16,27 @@ text1 and text2 consist of only lowercase English characters.
 """
 
 
+import unittest
+
+
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         return 0
 
 # use a dict of the longer string of {char: index}
+
+
+class Test(unittest.TestCase):
+    test_cases = (
+        ("abcde", "ace", 3),
+        ("abc", "abc", 3),
+        ("abc", "def", 0),
+        ("abcde", "cebcdea", 4),
+        ("ace", "abcde", 3)
+    )
+
+    def test_longestCommonSubsequence(self):
+        sol = Solution()
+        for text1, text2, expected in self.test_cases:
+            assert sol.longestCommonSubsequence(text1, text2) == expected
+            print(f"case {text1}, {text2} passed")
