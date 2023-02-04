f9d34889747b3f9dd312d087ef534e8680281e04
John Dugger <zathrath03@gmail.com>
2023-02-04 10:43:33 -0800

    Implement solutions

---
+++ 567. Permutation in String.py
@@ -0,0 +1,26 @@
+from collections import Counter
+
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l2 = len(s2)
+        l1 = len(s1)
+
+        if l1 > l2:
+            return False
+
+        s1_char_counts = Counter(s1)
+        s2_char_counts_in_window = Counter(s2[:l1])
+
+        for i in range(l1, l2):
+            if s1_char_counts == s2_char_counts_in_window:
+                return True
+            char_to_remove = s2[i - l1]
+            char_to_add = s2[i]
+            s2_char_counts_in_window[char_to_remove] -= 1
+            s2_char_counts_in_window[char_to_add] += 1
+
+        if s1_char_counts == s2_char_counts_in_window:
+            return True
+
+        return False
